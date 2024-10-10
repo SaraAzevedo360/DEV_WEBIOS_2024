2b6ad258003f44a0dbc0655818d395d10e8d84af
SaraAzevedo360 <azevedocordeiro.sa@gmail.com>
2024-10-10 20:35:03 -0300

    Aula JS01

---
+++ JS/JS01-IntroducaoJS/main.js
@@ -0,0 +1,6 @@
+// html <!-- -->
+// css --- /* */ 
+// java --- // Para comentar uma linha de codigo
+// java --- /* */ Para comentar duas linha o mais de codigo
+
+alert("Hello World! Estou começando aprender Java Script")
