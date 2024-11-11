f3261a0096ec94915613e2e4dcef349411dacaca
sara azevedo <azevedocordeiro.sa@gmail.com>
2024-11-10 22:25:59 -0300

    Casa

--- JS/JS09-MetodoArray/ExercicioCasa/main.js
+++ JS/JS09-MetodoArray/ExercicioCasa/main.js
@@ -12,3 +12,4 @@ numeros.forEach(ImparPar);
 let = numeros [17, 43, 8, 4, 97, 56, 29, 3, 75, 34, 88, 82, 61, 59];
 let Nfiltros = numeros.filter(numero => numero > 20 && numero < 80);
 console.log(Nfiltros);
+console.log(numeros);
